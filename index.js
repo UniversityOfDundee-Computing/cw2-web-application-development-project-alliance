c2cf3afa2f844ede19313e140a50c03879313581
MAC <mac@MACs-MacBook-Pro-2.local>
2025-04-23 23:29:34 +0100

    feat: working on the emoji - genre

---
+++ index.js
@@ -0,0 +1,23 @@
+const genres = [
+  { id: 1, genre: "Action", emoji: "🔫" },
+  { id: 2, genre: "Adventure", emoji: "🗺️" },
+  { id: 3, genre: "Comedy", emoji: "😂" },
+  { id: 4, genre: "Drama", emoji: "😢" },
+  { id: 5, genre: "Horror", emoji: "👻" },
+  { id: 6, genre: "Science Fiction", emoji: "🚀" },
+  { id: 7, genre: "Fantasy", emoji: "🐉" },
+  { id: 8, genre: "Romance", emoji: "💋" },
+];
+
+const container = document.getElementById("buttonContainer");
+container.innerHTML = "";
+
+genres.forEach((genre) => {
+  container.innerHTML += `
+      <div class="w-full flex flex-row justify-center items-center h-32">
+        <button class="text-7xl hover:bg-gray-500 p-5 rounded-full" onclick="filterMovies('${genre.genre}')">
+          ${genre.emoji}
+        </button>
+      </div>
+    `;
+});
